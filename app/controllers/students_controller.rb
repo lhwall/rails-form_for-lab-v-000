cf0e4a9438760196d356ccb9d1ab3aee56d5e3e4
Lucy Wall <lhwall@gmail.com>
2018-07-09 16:47:31 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,7 @@ class StudentsController < ApplicationController
   def create
     @student = Student.create({:first_name => params[:first_name], :last_name => params[:last_name]})
     @student.save
+    byebug
     redirect_to student_path(@student)
   end
 
