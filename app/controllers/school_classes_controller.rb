67e000555b7b408b0116fd16acbaed423cd1fab7
Lucy Wall <lhwall@gmail.com>
2018-07-09 16:29:45 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -23,7 +23,7 @@ class SchoolClassesController < ApplicationController
     @school_class = SchoolClass.find(params[:id])
     @school_class.update(params.require(:school_class).permit(:title, :room_number))
     @school_class.save
-    byebug
+    #byebug
     redirect_to school_class_path(@school_class)
   end
 
