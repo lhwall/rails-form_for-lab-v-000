6ba95fa0d2ab48ee444a4b39eb33a3ba4c9125b6
Lucy Wall <lhwall@gmail.com>
2018-07-09 16:42:30 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -20,7 +20,7 @@ class StudentsController < ApplicationController
   def update
     @student = Student.find(params[:id])
     @student.update(params.require(:student).permit(:first_name, :last_name))
-    @student.save 
+    @student.save
   end
 
 end
