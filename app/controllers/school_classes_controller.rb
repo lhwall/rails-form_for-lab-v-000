7c845022222879364e3620c2dc1acb2901691cb3
Lucy Wall <lhwall@gmail.com>
2018-07-09 14:45:07 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -7,7 +7,7 @@ class SchoolClassesController < ApplicationController
   def create
     @school_class = SchoolClass.create({:title => params[:school_class][:title], :room_number => params[:school_class][:room_number]})
     @school_class.save
-    byebug
+    #byebug
     redirect_to school_class_path(@school_class)
   end
 
